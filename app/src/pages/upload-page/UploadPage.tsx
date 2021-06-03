b49bf87c518918a2dc6155db6c7056ebf7c4e29f
Matheus <matheus.fachinis@gmail.com>
2021-06-03 19:33:48 -0300

    feat: upload page list the images to upload

--- app/src/pages/upload-page/UploadPage.tsx
+++ app/src/pages/upload-page/UploadPage.tsx
@@ -1,7 +1,42 @@
+import { useRef } from "react";
 import { useRecoilState } from "recoil";
-import { filesState } from "../../states/files";
-
+import { useDragInDrop } from "../../hooks/useDragInDrop";
+import { filesState, FileState } from "../../states/files";
+import "./upload-page.scss";
 export function UploadPage() {
     const [files, setFiles] = useRecoilState(filesState);
-    return <section className="upload-page">{JSON.stringify(files)}</section>;
+    const wrapperRef = useRef(null);
+    const { dropDiv } = useDragInDrop({
+        ref: wrapperRef,
+        onDrop: (data) => {
+            const filesToUpload: FileState[] = Array.from(data.files).map(
+                (file) => ({
+                    filename: file.name,
+                    size: file.size,
+                    blob: file,
+                    mimeType: file.type,
+                    url: URL.createObjectURL(file),
+                })
+            );
+
+            setFiles((prevState) => [...prevState, ...filesToUpload]);
+        },
+    });
+    return (
+        <section className="upload-page">
+            <div className="wrapper" ref={wrapperRef}>
+                <header></header>
+                {dropDiv || (
+                    <div className="files-list">
+                        {files.map((file) => (
+                            <img src={file.url} alt={file.filename} />
+                        ))}
+                    </div>
+                )}
+                <div className="send-files">
+                    <button>Upload dos arquivos</button>
+                </div>
+            </div>
+        </section>
+    );
 }
