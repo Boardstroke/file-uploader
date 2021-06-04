43e7e162a7ede8c527bac2ae928b8abdc3328dfc
Matheus <matheus.fachinis@gmail.com>
2021-06-03 22:42:50 -0300

    feat:  separate each file that can upload on route handler

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,16 @@
-import express, {json, urlencoded, raw} from "express"
+import express, { json, urlencoded, raw } from "express";
 import cors from "cors";
-import { fileUploadRouter } from "./components/file-upload/file-upload-router"
-import logger from "morgan"
-const app = express()
-app.use(cors)
-app.use(raw({type: "image/*", limit: '30mb'}))
-app.use(json())
-app.use(logger("dev"))
+import { fileUploadRouter } from "./components/file-upload/file-upload-router";
+import logger from "morgan";
+const app = express();
 
+app.use(cors({ exposedHeaders: "Location", origin: "*" }));
+app.use(raw({ type: "image/*", limit: "30mb" }));
+app.use(json());
+app.use(logger("dev"));
+app.use(fileUploadRouter);
+app.get("/", (req, res) => {
+  res.send({ message: "ok" });
+});
 
-export default app
+export default app;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,26 @@
 import http from "http";
 import app from "./app";
 import { Server } from "socket.io";
-
+import url from "url";
+import { fileProperties } from "./components/file-upload/fileProperities";
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/upload/teste" && req.method === "PUT") {
+  const possiblesFilesIds = fileProperties.keys();
+  const urls = [];
+  for (let fileId of possiblesFilesIds) {
+    urls.push(`/upload/${fileId}`);
+  }
+  console.log(urls);
+  if (!req.url) return;
+  if (urls.includes(req.url) && req.method === "PUT") {
     let data: Buffer = Buffer.from([]);
     req.on("data", (chunk: Buffer) => {
       data = Buffer.concat([data, chunk]);
+      console.log(data);
       io.emit("progress", data.byteLength);
     });
     req.on("end", () => {
+      res.statusCode = 201;
       res.end();
     });
   }
@@ -18,13 +28,13 @@ const server = http.createServer((req, res) => {
   app(req, res);
 });
 const io = new Server(server, {
-    cors: {
-        origin: '*'
-    }
+  cors: {
+    origin: "*",
+  },
 });
 io.on("connect", (socket) => {
-    socket.emit('event', "hello")
-})
+  socket.emit("event", "hello");
+});
 server.listen(8081, () => {
   console.log("Estou ouvindo");
 });
