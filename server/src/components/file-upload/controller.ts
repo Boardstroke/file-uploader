0bde6e440934a35948a0a3c0aa48ccae5c8417e8
Matheus <matheus.fachinis@gmail.com>
2021-06-03 22:42:05 -0300

    feat: creating unique file id to each file

--- server/src/components/file-upload/controller.ts
+++ server/src/components/file-upload/controller.ts
@@ -2,41 +2,34 @@ import { randomUUID } from "crypto";
 import { Request, Response } from "express";
 import { fileProperties } from "./fileProperities";
 import fs, { createReadStream } from "fs";
-export function setupUpload(req: Request, res: Response){
-    const fileId = randomUUID();
-    const header = req.header;
-    const contentLength = header("x-content-length");
-    const contentType = header("x-content-length");  
-    let filename = req.body.filename
-    const caption = req.body.caption;
-    
-    if(!contentLength || !contentType){
-        return res.sendStatus(400).send({
-            message: "Não foi possivel criar sessão"
-        }).end()
-    }
+export function setupUpload(req: Request, res: Response) {
+  const fileId = randomUUID();
+  const type = req.header("x-upload-content-type");
+  const size = req.header("x-upload-content-size");
+  if (!type || !size) {
+    return res.status(400).send({
+      message: "please, provide type and size of a file",
+    });
+  }
 
-    if(!filename) filename = fileId
+  const { filename } = req.body;
+  if (!filename) {
+    return res.status(400).send({
+      message: "please provide the filename",
+    });
+  }
 
-    
-
-    fileProperties.set(fileId, {contentLength, contentType, filename, caption})
+  fileProperties.set(fileId, {
+    contentType: type,
+    contentLength: size,
+    filename,
+  });
+  console.log(fileId);
+  res.setHeader("Location", `/upload/${fileId}`).status(200).send(req.body);
 }
 
-export function upload(req: Request, res: Response){
-    const {fileId} = req.params
-    const filePayload = fileProperties.get(fileId);
-    // const buffer = req.body as Buffer
-    req.on("data", (chunk) => {
-        console.log(chunk)
-    })
-    // fs.writeFile(`./uploads/teste.jpg`, buffer, () => {
-    //     console.log("pronto")
-    // })
-    
-    res.status(200).end();
+export function upload(req: Request, res: Response) {
+  res.status(200).end();
 }
 
-export function uploadStatus(req: Request, res: Response){
-    
-}
+export function uploadStatus(req: Request, res: Response) {}
